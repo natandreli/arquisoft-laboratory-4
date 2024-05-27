7367f85107a1df9b77f4fe25d70651af9146731c
natandreli <natalia.garcia9@udea.edu.co>
2024-05-27 10:40:09 -0500

    Add server

--- src/app/page.js
+++ src/app/page.js
@@ -3,15 +3,50 @@
 import { useState, useEffect } from "react";
 import GateInfoCard from "../components/GateInfoCard";
 import gateInfo from "../resources/gateInfo";
+import { socket } from "../socket";
 
 const Home = () => {
   const [gateData, setGateData] = useState(gateInfo);
 
-  const handleUpdate = (gate, newInfo) => {
-  };
+  const [isConnected, setIsConnected] = useState(false);
+  const [transport, setTransport] = useState("N/A");
+
+  useEffect(() => {
+    if (socket.connected) {
+      onConnect();
+    }
+
+    function onConnect() {
+      setIsConnected(true);
+      setTransport(socket.io.engine.transport.name);
+
+      socket.io.engine.on("upgrade", (transport) => {
+        setTransport(transport.name);
+      });
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+      setTransport("N/A");
+    }
+
+    socket.on("connect", onConnect);
+    socket.on("disconnect", onDisconnect);
+
+    return () => {
+      socket.off("connect", onConnect);
+      socket.off("disconnect", onDisconnect);
+    };
+  }, []);
+
+  const handleUpdate = (gate, newInfo) => {};
 
   return (
     <main className="min-h-screen">
+      <div>
+        <p>Status: {isConnected ? "connected" : "disconnected"}</p>
+        <p>Transport: {transport}</p>
+      </div>
       <div className="mb-10 flex flex-col items-center bg-transparent">
         <span className="w-auto inline-block text-5xl font-bold text-center bg-clip-text text-transparent bg-gradient-linear-1">
           SITAS
